a506cf513d9ea10a9b3eb1464f74754dbd46bc67
Daniel Torres <youkiddingd@gmail.com>
2017-03-10 12:05:23 -0600

    Cambios en clase

--- Bota y bota y no es pelota/Lienzo.java
+++ Bota y bota y no es pelota/Lienzo.java
@@ -2,30 +2,19 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.Timer;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 public class Lienzo extends JPanel
 {
    private Pelota pelota;
    public Lienzo()
    {
      pelota= new Pelota(400,300,35);
-     EscuchadorTiempo escuchaTiempo = new EscuchadorTiempo();
-     Timer tiempo = new Timer(1000,escuchaTiempo);
-     tiempo.start();
    }
-   
+  
    @Override
    public void paintComponent(Graphics g)
    {
        pelota.dibujate(g);
    }
-   
-   public class EscuchadorTiempo implements ActionListener
-   {
-       @Override
-       public void actionPerformed(ActionEvent e)
-       {
-           System.out.println("Hola pelota ;)");
-       }
    }
-}
--- Bota y bota y no es pelota/Ventana.java
+++ Bota y bota y no es pelota/Ventana.java
@@ -1,12 +1,51 @@
 import javax.swing.JFrame;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class Ventana extends JFrame
 {
    private Lienzo dibujo;
+   private Timer tiempo;
    public Ventana(int ancho, int alto)
    {
       this.setSize(ancho,alto);
+      EscuchadorTiempo escuchaTiempo = new EscuchadorTiempo();
+      tiempo = new Timer(1000,escuchaTiempo);
+      tiempo.start();
       dibujo= new Lienzo();
       this.add(dibujo);
+      EscuchadorTeclado esc = new EscuchadorTeclado();
+      this.addKeyListener(esc);
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    }
+    public class EscuchadorTiempo implements ActionListener
+   {
+       @Override
+       public void actionPerformed(ActionEvent e)
+       {
+           System.out.println("Hola pelota ;)");
+       }
+   }
+   public class EscuchadorTeclado implements KeyListener
+   {
+      @Override
+   public void keyPressed(KeyEvent e)
+   {
+       System.out.println("All you have to do is...");
+       if(e.getKeyChar() == 'd')
+          tiempo.stop();
+   }
+    @Override
+   public void keyReleased(KeyEvent e)
+   {
+       System.out.println("A minute");
+   }
+    @Override
+   public void keyTyped(KeyEvent e)
+   {
+       System.out.println("Stay");
+   }
+}
 }
